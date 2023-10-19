34e883c7530ad4aeeba9f3acd26d869c0ece743b
shigeru <proshigeru@gmail.com>
2023-10-19 14:19:00 -0300

    feat(server): added method update

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -1,35 +1,37 @@
 export class TodoController {
   todo = [
     {
-      "id": 1,
-      "name": "Aprender Node",
-      "status": "Done"
+      id: 1,
+      name: "Aprender Node",
+      status: "Done",
     },
     {
-      "id": 2,
-      "name": "Aprender Java",
-      "status": "Progress"
-    }
-  ]
+      id: 2,
+      name: "Aprender Java",
+      status: "Progress",
+    },
+  ];
 
   getAll() {
-    return this.todo
+    return this.todo;
   }
 
   getById(id: number) {
-    const data = this.todo.find(item => item.id === id);
+    const data = this.todo.find((item) => item.id === id);
     return data;
   }
 
   create(name: string, status: string) {
-    this.todo.push({ id: Math.floor(Math.random() * 1000), name, status })
+    this.todo.push({ id: Math.floor(Math.random() * 1000), name, status });
   }
 
-  update() {
+  update(id: number, name: string, status: string) {
+    const targetUpdate = this.todo.findIndex((item) => item.id === id);
 
+    if (targetUpdate !== -1) {
+      this.todo[targetUpdate] = { id, name, status };
+    }
   }
 
-  delete() {
-
-  }
+  delete() {}
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,23 +1,31 @@
 import { Router } from "express";
 import { TodoController } from "./controllers/todoController";
 
-export const router = Router()
+export const router = Router();
 
-const todoController = new TodoController()
+const todoController = new TodoController();
 
-router.get('/', (req, res) => {
-  const todo = todoController.getAll()
-  res.send(todo)
-})
+router.get("/", (req, res) => {
+  const todo = todoController.getAll();
+  res.send(todo);
+});
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  const todo = todoController.getById(Number(id))
-  res.send(todo)
-})
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = todoController.getById(Number(id));
+  res.send(todo);
+});
 
-router.post('/', (req, res) => {
-  const { name, status } = req.body
-  todoController.create( name, status)
-  res.send('Tarefa criada.')
-})
+router.post("/", (req, res) => {
+  const { name, status } = req.body;
+  todoController.create(name, status);
+  res.send("Tarefa criada.");
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, status } = req.body;
+
+  todoController.update(Number(id), name, status);
+  res.send("Tarefa atualizada");
+});
